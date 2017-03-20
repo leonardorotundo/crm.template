933aee203bb7c526e9aa8f202187dd4d259c82b2
leonardo <leonardo@link-b.com>
2017-03-20 18:05:27 -0300

    Cambios rutas

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -2,7 +2,6 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,21 +13,14 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
  * @author Leonardo Rotundo <leonardorotundo@gmail.com>
  */
 class AppController extends Controller {
-    /**
-     * @Route("/", name="index")
-     */
+    
     public function indexController(){
         $user = $this->getUser();
         if(is_null($user)){
             return $this->redirectToRoute("fos_user_security_login");
-        }  
+        }
         
-        return $this->redirectToRoute("show");
+//        return $this->redirectToRoute("show");
     }
-    /**
-     * @Route("/dashboard", name="show")
-     */
-    public function showController(){
-        
-    }
+    
 }
